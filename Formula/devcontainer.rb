9c6f67d311b2d32b4dd086ff5fdb403075490d6a
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-07 07:42:29 +0100

    Brew formula update for devcontainer-cli version v0.1.913729827

--- Formula/devcontainer.rb
+++ Formula/devcontainer.rb
@@ -5,16 +5,16 @@
 class Devcontainer < Formula
   desc "CLI for working with Visual Studio Code devcontainers"
   homepage "https://github.com/stuartleeks/devcontainer-cli"
-  version "0.1.901789955"
+  version "0.1.913729827"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/stuartleeks/devcontainer-cli/releases/download/v0.1.901789955/devcontainer-cli_darwin_amd64.tar.gz"
-    sha256 "a9962d02da996421e04ebdb3e52cd3ce80dac5b63b472e6277e3523e37c2787d"
+    url "https://github.com/stuartleeks/devcontainer-cli/releases/download/v0.1.913729827/devcontainer-cli_darwin_amd64.tar.gz"
+    sha256 "f8d880a480980e0226b6e68a2d541949c075dd3b5259403bb8fa7628ad459dec"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/stuartleeks/devcontainer-cli/releases/download/v0.1.901789955/devcontainer-cli_linux_amd64.tar.gz"
-    sha256 "50dd78329fef4f4d570535f1f4f1db4ac8ca8ab2fbf9a45e4a415b72190f48bd"
+    url "https://github.com/stuartleeks/devcontainer-cli/releases/download/v0.1.913729827/devcontainer-cli_linux_amd64.tar.gz"
+    sha256 "6907f10c02801ecbb689ff1dc2c138d63e504759ebeffd0c819e0ebd37271b63"
   end
 
   def install
